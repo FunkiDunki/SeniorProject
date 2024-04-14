68f83875b2840ae85741653deb20010358bd7283
FunkiDunki <nicholashotelling@gmail.com>
2024-04-14 14:39:24 -0700

    updates according to linter

--- src/dataclasses/population.py
+++ src/dataclasses/population.py
@@ -1,25 +1,34 @@
-import dataclasses as dc
 import numpy as np
+import dataclasses as dc
+
 
 @dc.dataclass
 class Population:
-    '''
+    """
     Populations have size, and a representation of current state.
     Ideally, the needs match the meanings of items.
-    '''
-    size: int #size is count of population
+    """
+
+    size: int  # size is count of population
     needs_state: np.array 
 
+
 def default_needs(len: int):
-    '''creates and returns a valid needs_state
+    """
+    Creates and returns a valid needs_state
     INPUT: how many need factors exist
     RETURN: a valid needs_state
-    '''
+    """
+
     return np.random.rand(len)
 
+
 def initialize_population(size: int) -> Population:
-    '''initialize a population with a certain size.
+    """
+    initialize a population with a certain size.
     INPUT: size
-    OUTPUT: population with given size, and default needs'''
+    OUTPUT: population with given size, and default needs
+    """
+
     pop = Population(size, default_needs(3))
     return pop
