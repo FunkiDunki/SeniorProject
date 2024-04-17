570b456f462627bf09355738d8d7a2310d8d0e5f
BPond4 <bpond@calpoly.edu>
2024-04-16 20:07:06 -0700

    Added recipe dataclass and modified employee dataclass

--- src/dataclasses/employee.py
+++ src/dataclasses/employee.py
@@ -1,13 +1,16 @@
 import dataclasses as dc
+from typing import List,  Tuple
 
 
 @dc.dataclass
 class Employee:
     """
     Employees are basic unit of scaling.
-    Each employee has name, and stats
+    Each employee has name, and stats, 
+    as well as a list of their skill tags.
     """
 
     name: str
     salary: int  # how much does this employee earn in one year
     morale: float  # used to change efficiency
+    tags: List[Tuple[str,int]]  #the list of skill tags where each one has an efficiency modifier between 0 and 100
