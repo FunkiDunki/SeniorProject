6da88c0f627b7904521c0de7eb0adf176a071391
NickPerlich <nicholasperlich2003@gmail.com>
2024-05-08 19:29:15 -0700

    created random employee function

--- server/src/datas/employee.py
+++ server/src/datas/employee.py
@@ -1,5 +1,7 @@
 import dataclasses as dc
 from typing import List, Tuple
+from faker import Faker
+import random
 
 
 @dc.dataclass
@@ -18,3 +20,13 @@ class Employee:
     efficiency modifier between 0 and 100
     """
     tags: List[Tuple[str, int]]
+
+
+def rand_employee() -> Employee:
+    faker = Faker()
+
+    name = faker.name()
+    salary = random.randint(1, 1000)
+    morale = random.uniform(1.0, 100.0)
+
+    return Employee(name, salary, morale, [])
