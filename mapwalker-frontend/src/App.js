ac7f198e1e23115c38fd6cac741034ab1e8ed255
NickPerlich <nicholasperlich2003@gmail.com>
2024-11-19 12:24:53 -0800

    initial frontend created

---
+++ mapwalker-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Adventure from "./Adventure";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "20px", backgroundColor: "#f4f4f9" }}>
+      <h1 style={{ textAlign: "center" }}>Unicorn Blood</h1>
+      <Adventure />
+    </div>
+  );
+}
+
+export default App;
